e838845d12114ddf2f5045b4e6d027dd7c642a50
smonek <simonjantosik@seznam.cz>
2023-04-22 19:53:06 +0200

    (02 - last push)

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-/* Připojení modulu frameworku Express */
-const express = require('express');
-/* Vytvoření základního objektu serverové aplikace */
+const express = require("express");
 const app = express();
-/* Nastavení portu, na němž bude spuštěný server naslouchat */
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const csvtojson = require('csvtojson');
+const fs = require("fs");
+const path = require("path");
 const port = 3000;
-/* Spuštění webového serveru */
-app.listen(port, () => { 
-    console.log(`Server naslouchá na portu ${port}`); 
-});
+app.use(express.static("mainsite"));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.listen(port, () =>{console.log(`Server naslouchá na portu ${port}`);});
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+app.post('/savedata', urlencodedParser, (req, res) =>{
+    let date = moment().format('DD-MM-YYYY');
+    let str = `"${req.body.ukol}","${req.body.predmet}","${date}","${req.body.odevzdani}"\n`;
+    fs.appendFile(path.join(__dirname, 'zaznam/ukoly.csv'), str, function(err) {
+        if(err) {
+            console.error(err);
+            returnres.status(400).json({success: false,message: "Nastala chyba během ukládání souboru"
+            });
+        }
+    });
+    res.redirect(301, '/');});
+    app.get("/zaznam", (req, res) => {
+        csvtojson({ headers: ['ukol', 'predmet', 'zadani', 'odevzdani'] })
+          .fromFile(path.join(__dirname, 'zaznam/ukoly.csv'))
+          .then(data => {
+            data.sort((a, b) => new Date(a.odevzdani) - new Date(b.odevzdani));
+            res.render('index', { nadpis: "Seznam úkolů", ukoly: data });
+          })
+          .catch(err => {
+            res.render('error', { nadpis: "Chyba v aplikaci", chyba: err });
+          });
+      });
